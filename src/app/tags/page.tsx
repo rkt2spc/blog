a7447252fd7c65fd52a164316aaeade960514097
Tuan Minh Nguyen <nmtuan.dev@gmail.com>
2023-07-17 20:40:55 +0800

    Fix tags page

--- src/app/tags/page.tsx
+++ src/app/tags/page.tsx
@@ -1,4 +1,4 @@
-// import PageLayout from '@/layouts/PageLayout'
+import PageLayout from '@/layouts/PageLayout'
 import Tag from '@/components/Tag'
 
 import { getAllPostsTag } from '@/lib/mdx'
@@ -8,13 +8,8 @@ export default async function TagsPage() {
   const sortedTags = Object.keys(tags).sort((a, b) => tags[b] - tags[a])
 
   return (
-    <div className="flex flex-col items-start justify-start divide-y divide-gray-200 dark:divide-gray-700 md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6 md:divide-y-0">
-      <div className="space-x-2 pt-6 pb-8 md:space-y-5">
-        <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:border-r-2 md:px-6 md:text-6xl md:leading-14">
-          Tags
-        </h1>
-      </div>
-      <div className="flex max-w-lg flex-wrap">
+    <PageLayout hLayout title="Tags">
+      <div className="flex flex-wrap md:max-w-lg">
         {Object.keys(tags).length === 0 && 'No tags found.'}
         {sortedTags.map((t) => {
           return (
@@ -24,6 +19,6 @@ export default async function TagsPage() {
           )
         })}
       </div>
-    </div>
+    </PageLayout>
   )
 }
--- src/layouts/PageLayout/index.tsx
+++ src/layouts/PageLayout/index.tsx
@@ -3,9 +3,24 @@ import { PropsWithChildren } from 'react'
 export type PageLayoutProps = PropsWithChildren & {
   title: string
   subtitle?: string
+  hLayout?: boolean
 }
 
-export default function PageLayout({ title, subtitle, children }: PageLayoutProps) {
+export default function PageLayout({ title, subtitle, children, hLayout }: PageLayoutProps) {
+  const containerCls = [
+    'flex flex-col divide-y',
+    'divide-gray-200 dark:divide-gray-700',
+    hLayout
+      ? 'md:flex-row md:divide-y-0 md:space-x-6 md:justify-center md:items-center md:mt-24 md:gap-x-4'
+      : '',
+  ]
+    .join(' ')
+    .trim()
+
+  const titleContainerCls = ['pb-3 space-y-1', hLayout ? 'md:border-r-2 md:pr-8 md:pb-0' : '']
+    .join(' ')
+    .trim()
+
   const titleCls = [
     'text-4xl md:text-5xl',
     'leading-9 sm:leading-10 md:leading-14',
@@ -17,13 +32,15 @@ export default function PageLayout({ title, subtitle, children }: PageLayoutProp
 
   const subtitleCls = ['text-lg leading-7', 'text-gray-500 dark:text-gray-400'].join(' ').trim()
 
+  const childrenContainerCls = ['pt-4', hLayout ? 'md:pt-0' : ''].join(' ').trim()
+
   return (
-    <div className="divide-y divide-gray-200 dark:divide-gray-700">
-      <div className="pb-3 space-y-1">
+    <div className={containerCls}>
+      <div className={titleContainerCls}>
         <h1 className={titleCls}>{title}</h1>
         <p className={subtitleCls}>{subtitle}</p>
       </div>
-      <div className="pt-4">{children}</div>
+      <div className={childrenContainerCls}>{children}</div>
     </div>
   )
 }
