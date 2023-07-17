8356e293c1677efb498fa850521aeb550ff40a3a
Tuan Minh Nguyen <nmtuan.dev@gmail.com>
2023-07-17 19:18:49 +0800

    NextJS build unlimited cache duration

--- src/lib/mdx/caches.ts
+++ src/lib/mdx/caches.ts
@@ -2,8 +2,8 @@ import 'server-only'
 import NodeCache from 'node-cache'
 
 export const cache = new NodeCache({
-  stdTTL: 1800,
-  checkperiod: 600,
+  stdTTL: 0,
+  checkperiod: 0,
   useClones: false,
   maxKeys: -1,
 })
